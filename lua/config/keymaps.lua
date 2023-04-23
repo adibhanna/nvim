c579dfb4dba77fc1c492b3dfec28900043a7bdde
Adib Hanna <adibhanna@gmail.com>
2023-04-23 16:37:39 -0500

    wip

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,10 +1,22 @@
 local opts = { noremap = true, silent = true }
 
-local keymap = vim.keymap.set
+local map = vim.keymap.set
 
-keymap("n", "<C-Space>", ":WhichKey \\<space><cr>", opts)
-keymap("n", "<C-d>", "<C-d>zz", opts)
-keymap("n", "<C-u>", "<C-u>zz", opts)
-keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
-keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
-keymap("n", "<leader><leader>", ":Telescope buffers<CR>", opts)
+map("n", "<C-Space>", ":WhichKey \\<space><cr>", opts)
+map("n", "<C-d>", "<C-d>zz", opts)
+map("n", "<C-u>", "<C-u>zz", opts)
+map("v", "J", ":m '>+1<CR>gv=gv", opts)
+map("v", "K", ":m '<-2<CR>gv=gv", opts)
+map("n", "<leader><leader>", ":Telescope buffers<CR>", opts)
+
+-- Remap for dealing with visual line wraps
+map("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+map("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+-- better indenting
+map("v", "<", "<gv")
+map("v", ">", ">gv")
+
+-- paste over currently selected text without yanking it
+map("v", "p", '"_dp')
+map("v", "P", '"_dP')
--- lua/plugins/modules/trouble.lua
+++ lua/plugins/modules/trouble.lua
@@ -1,35 +1,35 @@
 return {
-    "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true },
-    lazy = true,
-    config = function()
-        require("trouble").setup {
-            -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-            mode = "workspace_diagnostics",
-            position = "bottom", -- position of the list can be: bottom, top, left, right
-            height = 15,
-            padding = false,
-            action_keys = {
-                                            -- key mappings for actions in the trouble list
-                close = "q",                -- close the list
-                cancel = "<esc>",           -- cancel the preview and get back to your last window / buffer / cursor
-                refresh = "r",              -- manually refresh
-                jump = { "<cr>", "<tab>" }, -- jump to the diagnostic or open / close folds
-                open_split = { "<c-x>" },   -- open buffer in new split
-                open_vsplit = { "<c-v>" },  -- open buffer in new vsplit
-                open_tab = { "<c-t>" },     -- open buffer in new tab
-                jump_close = { "o" },       -- jump to the diagnostic and close the list
-                toggle_mode = "m",          -- toggle between "workspace" and "document" diagnostics mode
-                toggle_preview = "P",       -- toggle auto_preview
-                hover = "K",                -- opens a small popup with the full multiline message
-                preview = "p",              -- preview the diagnostic location
-                close_folds = { "zM" },     -- close all folds
-                open_folds = { "zR" },      -- open all folds
-                toggle_fold = { "za" },     -- toggle fold of current file
-            },
-            auto_jump = {},
-            use_diagnostic_signs = true,
-        }
-    end
+  "folke/trouble.nvim",
+  cmd = { "TroubleToggle", "Trouble" },
+  opts = { use_diagnostic_signs = true },
+  lazy = true,
+  config = function()
+    require("trouble").setup {
+      -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
+      mode = "workspace_diagnostics",
+      position = "bottom",       -- position of the list can be: bottom, top, left, right
+      height = 15,
+      padding = false,
+      action_keys = {
+        -- key mappings for actions in the trouble list
+        close = "q",                        -- close the list
+        cancel = "<esc>",                   -- cancel the preview and get back to your last window / buffer / cursor
+        refresh = "r",                      -- manually refresh
+        jump = { "<cr>", "<tab>" },         -- jump to the diagnostic or open / close folds
+        open_split = { "<c-x>" },           -- open buffer in new split
+        open_vsplit = { "<c-v>" },          -- open buffer in new vsplit
+        open_tab = { "<c-t>" },             -- open buffer in new tab
+        jump_close = { "o" },               -- jump to the diagnostic and close the list
+        toggle_mode = "m",                  -- toggle between "workspace" and "document" diagnostics mode
+        toggle_preview = "P",               -- toggle auto_preview
+        hover = "K",                        -- opens a small popup with the full multiline message
+        preview = "p",                      -- preview the diagnostic location
+        close_folds = { "zM" },             -- close all folds
+        open_folds = { "zR" },              -- open all folds
+        toggle_fold = { "za" },             -- toggle fold of current file
+      },
+      auto_jump = {},
+      use_diagnostic_signs = true,
+    }
+  end
 }
