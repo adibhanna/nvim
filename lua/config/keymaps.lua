58aea26f3ee5b137f0e691c0e710d3cd8dc7187f
Adib Hanna <adibhanna@gmail.com>
2023-04-23 14:23:02 -0500

    wip

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,8 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.keymap.set
 
 keymap("n", "<C-Space>", ":WhichKey \\<space><cr>", opts)
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
+keymap("n", "<leader><leader>", ":Telescope buffers<CR>", opts)
