a4f27e752b288909efbab728971fa78e43967fa7
Adib Hanna <adibhanna@gmail.com>
2023-05-27 11:31:01 -0500

    improvements

--- lua/plugins/modules/nvim-treesitter.lua
+++ lua/plugins/modules/nvim-treesitter.lua
@@ -3,7 +3,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
-    lazy = false,
     event = { "BufReadPost", "BufNewFile" },
     dependencies = {
         "nvim-treesitter/nvim-treesitter-textobjects",
