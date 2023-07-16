a14d4be6a3b09fd2238275c74a1870fd1cdc412a
Adib Hanna <adibhanna@gmail.com>
2023-07-16 00:18:56 -0500

    go stuff

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -4,12 +4,12 @@ if not status_ok then
 end
 
 local opts = {
-  mode = "n", -- NORMAL mode
+  mode = "n",     -- NORMAL mode
   prefix = "<leader>",
-  buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-  silent = true, -- use `silent` when creating keymaps
+  buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true,  -- use `silent` when creating keymaps
   noremap = true, -- use `noremap` when creating keymaps
-  nowait = true, -- use `nowait` when creating keymaps
+  nowait = true,  -- use `nowait` when creating keymaps
 }
 local mappings = {
   l = {
@@ -17,41 +17,15 @@ local mappings = {
   },
   g = {
     name = "+Go",
-    i = { "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies" },
-    o = { "<cmd>GoPkgOutline<cr>", "Outline" },
-    I = { "<cmd>GoToggleInlay<cr>", "Toggle inlay" },
-    -- l = { "<cmd>GoLint<cr>", "Run linter" },
-    g = { "<cmd>GoGenerate<Cr>", "Go Generate" },
-    G = { "<cmd>GoGenerate %<Cr>", "Go Generate File" },
-    c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
-    s = { "<cmd>GoFillStruct<cr>", "Autofill struct" },
-    j = { "<cmd>'<,'>GoJson2Struct<cr>", "Json to struct" },
-    h = {
-      name = "Helper",
-      a = { "<cmd>GoAddTag<cr>", "Add tags to struct" },
-      r = { "<cmd>GoRMTag<cr>", "Remove tags to struct" },
-      c = { "<cmd>GoCoverage<cr>", "Test coverage" },
-      v = { "<cmd>GoVet<cr>", "Go vet" },
-      t = { "<cmd>GoModTidy<cr>", "Go mod tidy" },
-      i = { "<cmd>GoModInit<cr>", "Go mod init" },
+    s = {
+      name = "+Stuct",
+      j = { "<cmd> GoTagAdd json <CR>", "Add json tag" },
+      y = { "<cmd> GoTagAdd yaml <CR>", "Add yaml tag" },
+      J = { "<cmd> GoTagRm json <CR>", "Remove json tag" },
+      Y = { "<cmd> GoTagRm yaml <CR>", "Remove yaml tag" },
     },
-    t = {
-      name = "Tests",
-      r = { "<cmd>GoTest<cr>", "Run tests" },
-      a = { "<cmd>GoAlt!<cr>", "Open alt file" },
-      s = { "<cmd>GoAltS!<cr>", "Open alt file in split" },
-      v = { "<cmd>GoAltV!<cr>", "Open alt file in vertical split" },
-      u = { "<cmd>GoTestFunc<cr>", "Run test for current func" },
-      f = { "<cmd>GoTestFile<cr>", "Run test for current file" },
-      T = { "<cmd>GoTestAdd<Cr>", "Add Test" },
-      A = { "<cmd>GoTestsAll<Cr>", "Add All Tests" },
-      e = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
-    },
-    x = {
-      name = "Code Lens",
-      l = { "<cmd>GoCodeLenAct<cr>", "Toggle Lens" },
-      a = { "<cmd>GoCodeAction<cr>", "Code Action" },
-    },
+    t = { "<cmd>GoMod tidy<cr>", "go mod tidy" },
+    e = { "<cmd>GoIfErr<cr>", "Generate if err" },
   },
 }
 
--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -157,6 +157,45 @@ return {
         },
       })
 
+      lspconfig.gopls.setup({
+        settings = {
+          gopls = {
+            gofumpt = true,
+            codelenses = {
+              gc_details = false,
+              generate = true,
+              regenerate_cgo = true,
+              run_govulncheck = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
+            },
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+            analyses = {
+              fieldalignment = true,
+              nilness = true,
+              unusedparams = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            usePlaceholders = true,
+            completeUnimported = true,
+            staticcheck = true,
+            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+            -- semanticTokens = true,
+          }
+        }
+      })
+
       lsp.setup()
 
       -- cmp icons
@@ -422,6 +461,7 @@ return {
   -- go
   {
     "ray-x/go.nvim",
+    enabled = false,
     dependencies = { -- optional packages
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
