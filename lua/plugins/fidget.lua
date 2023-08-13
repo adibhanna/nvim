6ae21c253bb5a6adb5c77fe1bf7dd45b42f77eff
Adib Hanna <adibhanna@gmail.com>
2023-08-12 20:30:20 -0500

    wip

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -2,6 +2,8 @@ return {
   "j-hui/fidget.nvim",
   branch = "legacy",
   config = function()
-    require('fidget').setup()
+    require('fidget').setup({
+      window = { blend = 0 },
+    })
   end
 }
