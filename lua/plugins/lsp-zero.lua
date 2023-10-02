33cc38c1c004907b7b9e2e030acada56a6998eed
Adib Hanna <adibhanna@gmail.com>
2023-10-01 20:27:54 -0500

    update lsp-zero to v3

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,16 +1,19 @@
 return {
   {
     'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    branch = 'v3.x',
     lazy = true,
-    config = function()
-      -- This is where you modify the settings for lsp-zero
-      -- Note: autocompletion settings will not take effect
-
-      require('lsp-zero.settings').preset({
-        name = 'recommended',
-      })
-    end
+    config = false,
+    init = function()
+      -- Disable automatic setup, we are doing it manually
+      vim.g.lsp_zero_extend_cmp = 0
+      vim.g.lsp_zero_extend_lspconfig = 0
+    end,
+  },
+  {
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = true,
   },
 
   -- Autocompletion
@@ -22,13 +25,8 @@ return {
       { 'onsails/lspkind.nvim' },
     },
     config = function()
-      -- Here is where you configure the autocompletion settings.
-      -- The arguments for .extend() have the same shape as `manage_nvim_cmp`:
-      -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#manage_nvim_cmp
-
       require('lsp-zero.cmp').extend()
 
-      -- And you can configure cmp even more, if you want to.
       local cmp         = require('cmp')
       local lspkind     = require('lspkind')
       local icons       = require('config.icons')
@@ -232,47 +230,161 @@ return {
     dependencies = {
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'williamboman/mason-lspconfig.nvim' },
-      {
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
       { 'b0o/schemastore.nvim' }
     },
     config = function()
-      -- This is where all the LSP shenanigans will live
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
 
-      local lsp = require('lsp-zero')
-
-      lsp.ensure_installed({
-        'tsserver',
-        'eslint',
-        'rust_analyzer',
-        'gopls',
-        'lua_ls',
-        'jsonls',
-        'bashls',
-        'vimls',
-        -- 'nomic_solidity'
-      })
-
-      lsp.on_attach(function(client, bufnr)
-        lsp.default_keymaps({ buffer = bufnr })
+      lsp_zero.on_attach(function(client, bufnr)
+        lsp_zero.default_keymaps({ buffer = bufnr })
         vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = true })
       end)
 
-      lsp.skip_server_setup({ 'rust_analyzer' })
 
-      lsp.set_server_config({
-        on_init = function(client)
-          client.server_capabilities.semanticTokensProvider = nil
-        end,
+      local lspconfig = require('lspconfig')
+
+      require('mason').setup({})
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          'tsserver',
+          'eslint',
+          'rust_analyzer',
+          'gopls',
+          'lua_ls',
+          'jsonls',
+          'bashls',
+          'vimls',
+        },
+        handlers = {
+          lsp_zero.default_setup,
+          -- lspconfig.lua_ls.setup({
+          --   settings = {
+          --     Lua = {
+          --       diagnostics = {
+          --         globals = { "vim", "custom_nvim" },
+          --       },
+          --       workspace = {
+          --         library = vim.api.nvim_get_runtime_file("", true),
+          --         checkThirdParty = false,
+          --         hint = { enable = true },
+          --         telemetry = { enable = false },
+          --       },
+          --     },
+          --   },
+          -- }),
+          lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+          end,
+          lspconfig.solidity.setup({
+            cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
+            filetypes = { "solidity", "sol" },
+            root_dir = require("lspconfig.util").find_git_ancestor,
+            single_file_support = true,
+          }),
+
+          lspconfig.jsonls.setup({
+            settings = {
+              json = {
+                schema = require('schemastore').json.schemas(),
+                validate = { enable = true },
+              }
+            }
+          }),
+
+          lspconfig.tsserver.setup({
+            root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
+            filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'svelte' },
+            cmd = { "typescript-language-server", "--stdio" },
+          }),
+
+          lspconfig.eslint.setup({
+            filestypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'svelte' },
+            settings = {
+              workingDirectory = { mode = 'auto' },
+              format = { enable = true },
+              lint = { enable = true },
+            },
+          }),
+
+          lspconfig.rust_analyzer.setup({
+            settings = {
+              ["rust-analyzer"] = {
+                lens = {
+                  enable = true,
+                },
+                cargo = {
+                  allFeatures = true,
+                  loadOutDirsFromCheck = true,
+                  runBuildScripts = true,
+                },
+                -- Add clippy lints for Rust.
+                check = {
+                  enable = true,
+                  allFeatures = true,
+                  command = "clippy",
+                  extraArgs = { "--no-deps" },
+                },
+                procMacro = {
+                  enable = true,
+                  ignored = {
+                    ["async-trait"] = { "async_trait" },
+                    ["napi-derive"] = { "napi" },
+                    ["async-recursion"] = { "async_recursion" },
+                  },
+                },
+              },
+            },
+          }),
+
+          lspconfig.gopls.setup({
+            settings = {
+              gopls = {
+                gofumpt = true,
+                codelenses = {
+                  gc_details = false,
+                  generate = true,
+                  regenerate_cgo = true,
+                  run_govulncheck = true,
+                  test = true,
+                  tidy = true,
+                  upgrade_dependency = true,
+                  vendor = true,
+                },
+                hints = {
+                  assignVariableTypes = true,
+                  compositeLiteralFields = true,
+                  compositeLiteralTypes = true,
+                  constantValues = true,
+                  functionTypeParameters = true,
+                  parameterNames = true,
+                  rangeVariableTypes = true,
+                },
+                analyses = {
+                  fieldalignment = true,
+                  nilness = true,
+                  unusedparams = true,
+                  unusedwrite = true,
+                  useany = true,
+                },
+                usePlaceholders = true,
+                completeUnimported = true,
+                staticcheck = true,
+                directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              }
+            }
+          }),
+          lspconfig.terraformls.setup({
+            cmd = { "terraform-ls", "serve" },
+            filetypes = { "terraform", "tf", "terraform-vars" },
+            -- root_dir = lspconfig.util.root_pattern(".terraform", ".git"),
+            root_dir = lspconfig.util.root_pattern("*.tf", "*.terraform", "*.tfvars", "*.hcl", "*.config"),
+          })
+        },
       })
 
-      lsp.nvim_workspace()
-
-      lsp.format_on_save({
+      lsp_zero.format_on_save({
         format_opts = {
           async = false,
           timeout_ms = 10000,
@@ -281,17 +393,14 @@ return {
           ['lua_ls'] = { 'lua' },
           ['rust_analyzer'] = { 'rust' },
           ['gopls'] = { 'go' },
-          -- if you have a working setup with null-ls
-          -- you can specify filetypes it can format.
-          -- ['null-ls'] = {'javascript', 'typescript'},
         }
       })
 
-      lsp.set_preferences({
+      lsp_zero.set_preferences({
         suggest_lsp_servers = false,
       })
 
-      lsp.set_sign_icons({
+      lsp_zero.set_sign_icons({
         error = "E",
         warn = "W",
         hint = "H",
@@ -313,131 +422,6 @@ return {
           prefix = "",
         },
       })
-
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim", "custom_nvim" },
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
-              hint = { enable = true },
-              telemetry = { enable = false },
-            },
-          },
-        },
-      })
-
-      lspconfig.solidity.setup({
-        cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
-        filetypes = { "solidity", "sol" },
-        root_dir = require("lspconfig.util").find_git_ancestor,
-        single_file_support = true,
-      })
-
-      lspconfig.jsonls.setup({
-        settings = {
-          json = {
-            schema = require('schemastore').json.schemas(),
-            validate = { enable = true },
-          }
-        }
-      })
-
-      lspconfig.tsserver.setup({
-        root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
-        filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'svelte' },
-        cmd = { "typescript-language-server", "--stdio" },
-      })
-
-      lspconfig.eslint.setup({
-        filestypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'svelte' },
-        settings = {
-          workingDirectory = { mode = 'auto' },
-          format = { enable = true },
-          lint = { enable = true },
-        },
-      })
-
-      lspconfig.rust_analyzer.setup({
-        settings = {
-          ["rust-analyzer"] = {
-            lens = {
-              enable = true,
-            },
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              runBuildScripts = true,
-            },
-            -- Add clippy lints for Rust.
-            check = {
-              enable = true,
-              allFeatures = true,
-              command = "clippy",
-              extraArgs = { "--no-deps" },
-            },
-            procMacro = {
-              enable = true,
-              ignored = {
-                ["async-trait"] = { "async_trait" },
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
-              },
-            },
-          },
-        },
-      })
-
-      lspconfig.gopls.setup({
-        settings = {
-          gopls = {
-            gofumpt = true,
-            codelenses = {
-              gc_details = false,
-              generate = true,
-              regenerate_cgo = true,
-              run_govulncheck = true,
-              test = true,
-              tidy = true,
-              upgrade_dependency = true,
-              vendor = true,
-            },
-            hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = true,
-              compositeLiteralTypes = true,
-              constantValues = true,
-              functionTypeParameters = true,
-              parameterNames = true,
-              rangeVariableTypes = true,
-            },
-            analyses = {
-              fieldalignment = true,
-              nilness = true,
-              unusedparams = true,
-              unusedwrite = true,
-              useany = true,
-            },
-            usePlaceholders = true,
-            completeUnimported = true,
-            staticcheck = true,
-            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-          }
-        }
-      })
-
-      -- lspconfig.terraformls.setup({
-      --   cmd = { "terraform-ls", "serve" },
-      --   filetypes = { "terraform", "tf", "terraform-vars" },
-      --   -- root_dir = lspconfig.util.root_pattern(".terraform", ".git"),
-      --   root_dir = lspconfig.util.root_pattern("*.tf", "*.terraform", "*.tfvars", "*.hcl", "*.config"),
-      -- })
-
-      lsp.setup()
     end
   }
 }
