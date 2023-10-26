6a966dbd36fbfe7954fac2ca0c4c1bbc07ba1168
Adib Hanna <adibhanna@gmail.com>
2023-10-26 10:01:33 -0500

    wip

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,40 +2,7 @@ return {
   "lewis6991/gitsigns.nvim",
   lazy = false,
   config = function()
-    local icons = require("config.icons")
     require("gitsigns").setup {
-      signs = {
-        add = {
-          hl = "GitSignsAdd",
-          text = icons.ui.BoldLineLeft,
-          numhl = "GitSignsAddNr",
-          linehl = "GitSignsAddLn",
-        },
-        change = {
-          hl = "GitSignsChange",
-          text = icons.ui.BoldLineLeft,
-          numhl = "GitSignsChangeNr",
-          linehl = "GitSignsChangeLn",
-        },
-        delete = {
-          hl = "GitSignsDelete",
-          text = icons.ui.Triangle,
-          numhl = "GitSignsDeleteNr",
-          linehl = "GitSignsDeleteLn",
-        },
-        topdelete = {
-          hl = "GitSignsDelete",
-          text = icons.ui.Triangle,
-          numhl = "GitSignsDeleteNr",
-          linehl = "GitSignsDeleteLn",
-        },
-        changedelete = {
-          hl = "GitSignsChange",
-          text = icons.ui.BoldLineLeft,
-          numhl = "GitSignsChangeNr",
-          linehl = "GitSignsChangeLn",
-        },
-      },
       signcolumn = true,
       numhl = false,
       linehl = false,
