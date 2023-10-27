3a01dea7758626044ec958cd607f72fc1a06635f
Adib Hanna <adibhanna@gmail.com>
2023-10-26 20:54:42 -0500

    wip

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -149,74 +149,10 @@ return {
           { name = "tmux" },
         },
         mapping = {
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-          ["<C-k>"] = cmp_mapping(cmp_mapping.select_prev_item(), { "i", "c" }),
-          ["<C-j>"] = cmp_mapping(cmp_mapping.select_next_item(), { "i", "c" }),
-          ["<Down>"] = cmp_mapping(cmp_mapping.select_next_item { behavior = cmp_types.SelectBehavior.Select }, {
-            "i" }),
-          ["<Up>"] = cmp_mapping(cmp_mapping.select_prev_item { behavior = cmp_types.SelectBehavior.Select }, {
-            "i" }),
-          ["<C-d>"] = cmp_mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp_mapping.scroll_docs(4),
-          ["<C-y>"] = cmp_mapping {
-            i = cmp_mapping.confirm { behavior = cmp_types.ConfirmBehavior.Replace, select = false },
-            c = function(fallback)
-              if cmp.visible() then
-                cmp.confirm { behavior = cmp_types.ConfirmBehavior.Replace, select = false }
-              else
-                fallback()
-              end
-            end,
-          },
-          ["<Tab>"] = cmp_mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            elseif utils.jumpable(1) then
-              luasnip.jump(1)
-            elseif utils.has_words_before() then
-              -- cmp.complete()
-              fallback()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp_mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          ['<Tab>'] = cmp_action.luasnip_supertab(),
+          ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
           ["<C-Space>"] = cmp_mapping.complete(),
-          ["<C-e>"] = cmp_mapping.abort(),
-          ["<CR>"] = cmp_mapping(function(fallback)
-            if cmp.visible() then
-              local confirm_opts = vim.deepcopy({
-                behavior = cmp_types.ConfirmBehavior.Replace,
-                select = false,
-              }) -- avoid mutating the original opts below
-              local is_insert_mode = function()
-                return vim.api.nvim_get_mode().mode:sub(1, 1) == "i"
-              end
-              if is_insert_mode() then -- prevent overwriting brackets
-                confirm_opts.behavior = cmp_types.ConfirmBehavior.Insert
-              end
-              local entry = cmp.get_selected_entry()
-              local is_copilot = entry and entry.source.name == "copilot"
-              if is_copilot then
-                confirm_opts.behavior = cmp_types.ConfirmBehavior.Replace
-                confirm_opts.select = true
-              end
-              if cmp.confirm(confirm_opts) then
-                return -- success, exit early
-              end
-            end
-            fallback() -- if not exited early, always fallback
-          end),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
         }
       })
     end
@@ -239,14 +175,13 @@ return {
       lsp_zero.on_attach(function(client, bufnr)
         lsp_zero.default_keymaps({ buffer = bufnr })
         opts = { buffer = bufnr, silent = true }
-        vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
-        -- vim.keymap.set('n', 'gd', 'lua vim.lsp.buf.definition()<cr>', { buffer = bufnr })
 
         vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
         vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
         vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
         vim.keymap.set('n', 'gi', ':Telescope lsp_implementations<cr>', opts)
         vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
         -- vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
         vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
         vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
@@ -275,25 +210,21 @@ return {
         },
         handlers = {
           lsp_zero.default_setup,
-          -- lspconfig.lua_ls.setup({
-          --   settings = {
-          --     Lua = {
-          --       diagnostics = {
-          --         globals = { "vim", "custom_nvim" },
-          --       },
-          --       workspace = {
-          --         library = vim.api.nvim_get_runtime_file("", true),
-          --         checkThirdParty = false,
-          --         hint = { enable = true },
-          --         telemetry = { enable = false },
-          --       },
-          --     },
-          --   },
-          -- }),
-          lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
-          end,
+          lspconfig.lua_ls.setup({
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim", "custom_nvim" },
+                },
+                workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                  checkThirdParty = false,
+                  hint = { enable = true },
+                  telemetry = { enable = false },
+                },
+              },
+            },
+          }),
           lspconfig.solidity.setup({
             cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
             filetypes = { "solidity", "sol" },
