583e18e13b08b790a25e51fe0c6705826a720251
Adib Hanna <adibhanna@gmail.com>
2023-11-10 14:09:29 -0600

    wip

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -139,6 +139,16 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   end,
 })
 
+
+--fix terraform and hcl comment string
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("FixTerraformCommentString", { clear = true }),
+  callback = function(ev)
+    vim.bo[ev.buf].commentstring = "# %s"
+  end,
+  pattern = { "terraform", "hcl" },
+})
+
 -- vim.api.nvim_create_autocmd({ "OptionSet" }, {
 --   pattern = { "background" },
 --   callback = function(ev)
