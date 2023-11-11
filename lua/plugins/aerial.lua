cd20d4ae91c7b1576cc411528a8f2e99b5da5bda
Adib Hanna <adibhanna@gmail.com>
2023-11-10 21:32:11 -0600

    wip

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -10,13 +10,11 @@ return {
     require("aerial").setup({
 
       backends = { "lsp", "treesitter", "markdown", "man" },
-      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
       on_attach = function(bufnr)
         -- Jump forwards/backwards with '{' and '}'
         vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
         vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
       end,
     })
-    -- You probably also want to set a keymap to toggle aerial
   end
 }
