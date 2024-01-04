9527b738bc5d5a85c39285e7db1f108d8da8943a
Adib Hanna <adibhanna@gmail.com>
2024-01-03 23:00:34 -0600

    new stuff

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -113,17 +113,19 @@ return {
       vim.g.gruvbox_material_float_style = 'dim'  -- Background of floating windows
       vim.g.gruvbox_material_statusline_style = 'material'
 
-      -- if (vim.o.background == 'light') then
-      --   vim.g.gruvbox_material_colors_override = { bg0 = '#f9f5d7' }
-      -- else
-      --   vim.g.gruvbox_material_colors_override = { bg0 = '#0e1010' }
-      -- end
-
-      vim.g.gruvbox_material_colors_override = { bg0 = '#000000' } -- #0e1010
-      -- vim.g.gruvbox_material_colors_override = { bg0 = '#121212' }
+      -- vim.g.gruvbox_material_colors_override = { bg0 = '#000000' } -- #0e1010
+      vim.g.gruvbox_material_colors_override = { bg0 = '#121212' }
       vim.g.gruvbox_material_better_performance = 1
       vim.cmd.colorscheme 'gruvbox-material'
     end,
+  },
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- require 'nordic'.load()
+    end
   },
   {
     "rose-pine/neovim",
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,74 +1,81 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  lazy = false,
-  config = function()
-    local icons = require('config.icons')
-    require("gitsigns").setup {
-      signs = {
-        add = {
-          hl = "GitSignsAdd",
-          text = icons.ui.BoldLineLeft,
-          numhl = "GitSignsAddNr",
-          linehl = "GitSignsAddLn",
+  {
+    "lewis6991/gitsigns.nvim",
+    lazy = false,
+    config = function()
+      local icons = require('config.icons')
+      require("gitsigns").setup {
+        signs = {
+          add = {
+            hl = "GitSignsAdd",
+            text = icons.ui.BoldLineLeft,
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn",
+          },
+          change = {
+            hl = "GitSignsChange",
+            text = icons.ui.BoldLineLeft,
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn",
+          },
+          delete = {
+            hl = "GitSignsDelete",
+            text = icons.ui.TriangleShortArrowRight,
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn",
+          },
+          topdelete = {
+            hl = "GitSignsDelete",
+            text = icons.ui.TriangleShortArrowRight,
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn",
+          },
+          changedelete = {
+            hl = "GitSignsChange",
+            text = icons.ui.BoldLineLeft,
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn",
+          },
         },
-        change = {
-          hl = "GitSignsChange",
-          text = icons.ui.BoldLineLeft,
-          numhl = "GitSignsChangeNr",
-          linehl = "GitSignsChangeLn",
+        signcolumn = true,
+        numhl = false,
+        linehl = false,
+        word_diff = false,
+        watch_gitdir = {
+          interval = 1000,
+          follow_files = true,
         },
-        delete = {
-          hl = "GitSignsDelete",
-          text = icons.ui.TriangleShortArrowRight,
-          numhl = "GitSignsDeleteNr",
-          linehl = "GitSignsDeleteLn",
+        attach_to_untracked = true,
+        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+          delay = 1000,
+          ignore_whitespace = false,
         },
-        topdelete = {
-          hl = "GitSignsDelete",
-          text = icons.ui.TriangleShortArrowRight,
-          numhl = "GitSignsDeleteNr",
-          linehl = "GitSignsDeleteLn",
+        current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+        sign_priority = 6,
+        status_formatter = nil,
+        update_debounce = 200,
+        max_file_length = 40000,
+        preview_config = {
+          border = "rounded",
+          style = "minimal",
+          relative = "cursor",
+          row = 0,
+          col = 1,
         },
-        changedelete = {
-          hl = "GitSignsChange",
-          text = icons.ui.BoldLineLeft,
-          numhl = "GitSignsChangeNr",
-          linehl = "GitSignsChangeLn",
-        },
-      },
-      signcolumn = true,
-      numhl = false,
-      linehl = false,
-      word_diff = false,
-      watch_gitdir = {
-        interval = 1000,
-        follow_files = true,
-      },
-      attach_to_untracked = true,
-      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
-        ignore_whitespace = false,
-      },
-      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-      sign_priority = 6,
-      status_formatter = nil,
-      update_debounce = 200,
-      max_file_length = 40000,
-      preview_config = {
-        border = "rounded",
-        style = "minimal",
-        relative = "cursor",
-        row = 0,
-        col = 1,
-      },
-      yadm = { enable = false },
+        yadm = { enable = false },
 
-      on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>H', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
-      end,
-    }
-  end
+        on_attach = function(bufnr)
+          vim.keymap.set('n', '<leader>H', require('gitsigns').preview_hunk,
+            { buffer = bufnr, desc = 'Preview git hunk' })
+        end,
+      }
+    end
+  },
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
 }
