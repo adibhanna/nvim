fd3a6e1ef626827734f4ae88fcfc232ac62b453f
Adib Hanna <adibhanna@gmail.com>
2024-01-04 23:47:15 -0600

    wip

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -9,8 +9,6 @@ return {
       "leoluz/nvim-dap-go"
     },
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
       require("go").setup({
         lsp_cfg = {
           settings = {
