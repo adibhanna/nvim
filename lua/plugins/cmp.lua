9db2713a2247c21a93f376bff616c0d4997d9e26
Adib Hanna <adibhanna@gmail.com>
2024-03-05 11:00:12 -0600

    wip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -67,6 +67,7 @@ return {
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete({}),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<CR>"] = cmp.mapping.confirm({
           behavior = cmp.ConfirmBehavior.Replace,
           select = true,
