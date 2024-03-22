d74fca1806ca4f91417202814c2626199aedbfa3
Adib Hanna <adibhanna@gmail.com>
2024-03-21 22:03:46 -0500

    wip

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -6,6 +6,7 @@ return {
         "theHamsta/nvim-dap-virtual-text",
         "nvim-telescope/telescope-dap.nvim",
         "folke/neodev.nvim",
+        "nvim-neotest/nvim-nio"
     },
     config = function()
         local dap = require("dap")
