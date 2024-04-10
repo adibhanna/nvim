b61505c8c1e3a702914b883ad050dc15cbe88c03
Adib Hanna <adibhanna@gmail.com>
2024-04-10 08:27:11 -0500

    wip

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,6 +1,6 @@
 return {
     "folke/todo-comments.nvim",
-    enabled = true,
+    enabled = false,
     dependencies = "nvim-lua/plenary.nvim",
     cmd = { "TodoTrouble", "TodoTelescope" },
     event = { "BufReadPost", "BufNewFile" },
