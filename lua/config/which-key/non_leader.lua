bfd3638904c137d22f1eadaac8ed0502f247dafb
Adib Hanna <adibhanna@gmail.com>
2024-07-14 09:22:39 -0500

    wip

--- lua/config/which-key/non_leader.lua
+++ lua/config/which-key/non_leader.lua
@@ -1,8 +1,8 @@
 return {
-  gd = "Goto definition",
-  gD = "Goto declaration",
-  gi = "Goto implementation",
-  gl = "Goto float diagnostics",
-  go = "Goto type definition",
-  gr = "Goto references",
+  { "gD", desc = "Goto declaration" },
+  { "gd", desc = "Goto definition" },
+  { "gi", desc = "Goto implementation" },
+  { "gl", desc = "Goto float diagnostics" },
+  { "go", desc = "Goto type definition" },
+  { "gr", desc = "Goto references" },
 }
