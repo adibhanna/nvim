5c6958b7e647a05486b23025dad69294b22bb86f
Adib Hanna <adibhanna@gmail.com>
2024-09-03 14:56:43 -0500

    wip

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,46 @@
+-- https://github.com/yetone/avante.nvim
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  opts = {
+    -- add any opts here
+    provider = "copilot",
+  },
+  -- if you want to download pre-built binary, then pass source=false. Make sure to follow instruction above.
+  -- Also note that downloading prebuilt binary is a lot faster comparing to compiling from source.
+  build = ":AvanteBuild source=false",
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "echasnovski/mini.icons",
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
