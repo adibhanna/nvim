d6dcf269b03c8e1beab2ff5201c696ef6b06b5d5
Adib Hanna <adibhanna@gmail.com>
2024-10-22 14:37:05 -0500

    wip

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -329,4 +329,24 @@ return {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
   },
+  {
+    "nvchad/showkeys",
+    cmd = "ShowkeysToggle",
+    opts = {
+      timeout = 1,
+      maxkeys = 6,
+      -- bottom-left, bottom-right, bottom-center, top-left, top-right, top-center
+      position = "bottom-right",
+    },
+
+    keys = {
+      {
+        "<leader>sk",
+        function()
+          vim.cmd("ShowkeysToggle")
+        end,
+        desc = "Show key presses",
+      },
+    },
+  },
 }
