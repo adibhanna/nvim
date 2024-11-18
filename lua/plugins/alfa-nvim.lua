867bcb9c66ff58081e9c64d45079ff0dec6a174e
Adib Hanna <adibhanna@gmail.com>
2024-11-18 10:10:25 -0600

    wip

--- lua/plugins/alfa-nvim.lua
+++ lua/plugins/alfa-nvim.lua
@@ -39,10 +39,10 @@ return {
     dashboard.section.buttons.val = {
       dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
       -- dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+      -- dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
       -- dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
       dashboard.button("s", " " .. "Restore Session", '<cmd>lua require("persistence").load()<cr>'),
-      dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/ <CR>"),
+      -- dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/ <CR>"),
       -- dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
       dashboard.button("q", " " .. " Quit", ":qa<CR>"),
     }
