74d13760eb4aa151a0603ffeae35a703c96b6cf6
Adib Hanna <adibhanna@gmail.com>
2024-11-18 09:47:21 -0600

    wip

--- lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "akinsho/bufferline.nvim",
-  enabled = false,
-  version = "*",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    require("bufferline").setup()
-  end,
-}
