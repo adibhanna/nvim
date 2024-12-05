16d9f40de7c54a0531c41b807b3db4317a363432
Adib Hanna <adibhanna@gmail.com>
2024-12-05 14:30:37 -0600

    wip

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 local opts = { noremap = true, silent = true }
 -- local map = vim.keymap.set
 
-
 -- Keep cursor centered when scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
 vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
