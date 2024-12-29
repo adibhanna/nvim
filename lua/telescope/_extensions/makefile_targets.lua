b236fe95c3d843ed181a41ccb6ef40f42bc857ca
عين <elkhatibomar@outlook.com>
2024-12-29 15:54:17 +0200

    Remove deprecated method nvim_buf_set_option

--- lua/telescope/_extensions/makefile_targets.lua
+++ lua/telescope/_extensions/makefile_targets.lua
@@ -18,21 +18,24 @@ end
 local function run_make_command(target)
   local command = "make " .. target
   local output = vim.fn.system(command)
-  -- Define the buffer name
   local buf_name = "Make Output: " .. target
+  
   -- Check if the buffer already exists and delete it
   for _, buf_id in ipairs(vim.api.nvim_list_bufs()) do
     if vim.api.nvim_buf_is_loaded(buf_id) and vim.api.nvim_buf_get_name(buf_id):match(vim.pesc(buf_name)) then
       vim.api.nvim_buf_delete(buf_id, { force = true })
     end
   end
+  
   -- Open a split at the bottom
   vim.cmd("botright new")
   local buf = vim.api.nvim_get_current_buf()
+  
   -- Rename the new buffer to avoid overwriting the previous buffer
   vim.api.nvim_buf_set_name(buf, buf_name)
   vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(output, "\n"))
-  vim.api.nvim_buf_set_option(buf, "modifiable", false)
+  vim.api.nvim_set_option_value("modifiable", false, { buf = buf })
+  
   -- Set the buffer to be deleted on quit
   vim.api.nvim_create_autocmd("BufLeave", {
     buffer = buf,
