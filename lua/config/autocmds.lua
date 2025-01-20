e6c0aabd93ccb3aad9c00b99573cf7b7d5809506
Adib Hanna <adibhanna@gmail.com>
2025-01-20 11:27:53 -0600

    wip

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -91,6 +91,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
     vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
 
+
     vim.cmd("highlight Winbar guibg=none")
 
     -- vim.cmd("highlight Comment guifg=#475558")
--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -466,4 +466,40 @@ return {
       -- vim.cmd.colorscheme("yugen")
     end,
   },
+
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        terminal_colors = true,
+        undercurl = false,
+        underline = false,
+        bold = false,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true,
+        contrast = "hard",
+        palette_overrides = {},
+        overrides = {
+          SignColumn = { bg = "#1C2021" },
+        },
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+
+      -- vim.cmd.colorscheme("gruvbox")
+    end,
+    opts = {},
+  },
 }
