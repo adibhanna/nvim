2907d70aa80d46dfb3e0519be0de6283d70e2a9e
Adib Hanna <adibhanna@gmail.com>
2025-02-10 16:11:50 -0600

    change stuff

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -119,11 +119,11 @@ vim.api.nvim_create_autocmd("FileType", {
 -- })
 --
 -- Run gofmt + goimport on save
-local goimport_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    require("go.format").goimport()
-  end,
-  group = goimport_sync_grp,
-})
+-- local goimport_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.go",
+--   callback = function()
+--     require("go.format").goimport()
+--   end,
+--   group = goimport_sync_grp,
+-- })
