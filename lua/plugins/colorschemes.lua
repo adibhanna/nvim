0fb948dd92e600d607530f23981a69be2b0e624b
Adib Hanna <adibhanna@gmail.com>
2025-02-19 10:01:02 -0600

    wip

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -376,7 +376,7 @@ return {
       -- vim.g.gruvbox_material_colors_override = { bg0 = '#16181A' } -- #0e1010
       -- vim.g.gruvbox_material_better_performance = 1
 
-      -- vim.cmd.colorscheme("gruvbox-material")
+      vim.cmd.colorscheme("gruvbox-material")
     end,
   },
   {
@@ -621,7 +621,7 @@ return {
         undercurl = false,
         transparent_background = true,
       })
-      vim.cmd.colorscheme("night-owl")
+      -- vim.cmd.colorscheme("night-owl")
     end,
   },
 }
