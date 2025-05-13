25f977e93ccc3f338a1e3fab1dece61a6c6bc588
Adib Hanna <adibhanna@gmail.com>
2025-05-13 11:19:12 -0500

    wip

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -376,7 +376,7 @@ return {
       -- vim.g.gruvbox_material_colors_override = { bg0 = '#16181A' } -- #0e1010
       -- vim.g.gruvbox_material_better_performance = 1
 
-      vim.cmd.colorscheme("gruvbox-material")
+      -- vim.cmd.colorscheme("gruvbox-material")
     end,
   },
   {
@@ -611,4 +611,66 @@ return {
       -- vim.cmd.colorscheme("vague")
     end
   },
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup({
+        transparent_background = false,
+        terminal_colors        = true,
+        devicons               = true, -- highlight the icons of `nvim-web-devicons`
+        styles                 = {
+          comment = { italic = false },
+          keyword = { italic = false },       -- any other keyword
+          type = { italic = false },          -- (preferred) int, long, char, etc
+          storageclass = { italic = false },  -- static, register, volatile, etc
+          structure = { italic = false },     -- struct, union, enum, etc
+          parameter = { italic = false },     -- parameter pass in function
+          annotation = { italic = false },
+          tag_attribute = { italic = false }, -- attribute of tag in reactjs
+        },
+        filter                 = "pro",       -- classic | octagon | pro | machine | ristretto | spectrum
+        -- Enable this will disable filter option
+        day_night              = {
+          enable = false,                      -- turn off by default
+          day_filter = "pro",                  -- classic | octagon | pro | machine | ristretto | spectrum
+          night_filter = "spectrum",           -- classic | octagon | pro | machine | ristretto | spectrum
+        },
+        inc_search             = "background", -- underline | background
+        background_clear       = {
+          -- "float_win",
+          -- "toggleterm",
+          -- "telescope",
+          "which-key",
+          -- "renamer",
+          "notify",
+          -- "nvim-tree",
+          -- "neo-tree",
+          -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+        }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+        plugins                = {
+          bufferline = {
+            underline_selected = false,
+            underline_visible = false,
+          },
+          indent_blankline = {
+            context_highlight = "default", -- default | pro
+            context_start_underline = false,
+          },
+        },
+        override               = function(c)
+          local hp = require("monokai-pro.color_helper")
+          local common_fg = hp.lighten(c.sideBar.foreground, 30)
+          return {
+            SnacksPicker = { bg = c.editor.background, fg = common_fg },
+            SnacksPickerBorder = { bg = c.editor.background, fg = c.tab.unfocusedActiveBorder },
+            SnacksPickerTree = { fg = c.editorLineNumber.foreground },
+            NonText = { fg = c.base.dimmed3 }, -- not sure if this should be broken into all hl groups importing NonText
+          }
+        end,
+      })
+
+      -- vim.cmd([[colorscheme monokai-pro-spectrum]])
+      vim.cmd.colorscheme("monokai-pro-spectrum")
+    end
+  }
 }
