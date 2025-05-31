b2c5fcb14bacebeb33a5e2247cdad6d39e00aa1d
Adib Hanna <adibhanna@gmail.com>
2025-05-31 10:55:46 -0500

    wip

--- lsp/lua-ls.lua
+++ lsp/lua-ls.lua
@@ -1,11 +1,21 @@
 local blink = require("blink.cmp")
 return {
     cmd = { "lua-language-server" },
-    root_markers = { ".luarc.json", ".luarc.jsonc" },
     filetypes = { "lua" },
+    root_markers = {
+        '.luarc.json',
+        '.luarc.jsonc',
+        '.luacheckrc',
+        '.stylua.toml',
+        'stylua.toml',
+        'selene.toml',
+        'selene.yml',
+        '.git',
+    },
     settings = {
         Lua = {
             diagnostics = {
+                disable = { "missing-fields" },
                 globals = {
                     "vim",
                     "Snacks",
