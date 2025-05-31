597585549a9df2e10162eac0fb6597e7fb68842b
Adib Hanna <adibhanna@gmail.com>
2025-05-31 16:16:00 -0500

    lsp stuff

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -29,3 +29,26 @@ vim.diagnostic.config({
         },
     },
 })
+
+local function restart_lsp(bufnr)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    local clients
+    if vim.lsp.get_clients then
+        clients = vim.lsp.get_clients({ bufnr = bufnr })
+    else
+        ---@diagnostic disable-next-line: deprecated
+        clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+    end
+
+    for _, client in ipairs(clients) do
+        vim.lsp.stop_client(client.id)
+    end
+
+    vim.defer_fn(function()
+        vim.cmd('edit')
+    end, 100)
+end
+
+vim.api.nvim_create_user_command('LspRestart', function()
+    restart_lsp()
+end, {})
