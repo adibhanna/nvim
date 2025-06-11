c3c51e89b5fbebb7b24522ab3078324f90e2d138
Adib Hanna <adibhanna@gmail.com>
2025-06-11 13:48:49 -0500

    wip

---
+++ lua/plugins/laravel.lua
@@ -0,0 +1,10 @@
+return {
+  'adibhanna/laravel.nvim',
+  ft = { 'php', 'blade' },
+  dependencies = {
+    'folke/snacks.nvim', -- Optional: for enhanced UI
+  },
+  config = function()
+    -- Plugin will auto-configure when entering a Laravel project
+  end,
+}
