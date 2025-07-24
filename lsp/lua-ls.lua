02f7c9bb970fd589dcdf5fa0c91d388ce321db94
Adib Hanna <adibhanna@gmail.com>
2025-07-24 13:34:07 -0500

    wip

--- lsp/lua-ls.lua
+++ lsp/lua-ls.lua
@@ -31,16 +31,16 @@ return {
             },
         },
     },
-    capabilities = vim.tbl_deep_extend(
-        "force",
-        {},
-        vim.lsp.protocol.make_client_capabilities(),
-        blink.get_lsp_capabilities(),
-        {
-            fileOperations = {
-                didRename = true,
-                willRename = true,
-            },
-        }
-    ),
+    -- capabilities = vim.tbl_deep_extend(
+    --     "force",
+    --     {},
+    --     vim.lsp.protocol.make_client_capabilities(),
+    --     blink.get_lsp_capabilities(),
+    --     {
+    --         fileOperations = {
+    --             didRename = true,
+    --             willRename = true,
+    --         },
+    --     }
+    -- ),
 }
