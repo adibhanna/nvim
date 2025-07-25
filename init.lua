b2d7b7536b78a4837be3e9f5d9daaafd5c92bf5b
Adib Hanna <adibhanna@gmail.com>
2025-07-25 09:38:11 -0500

    wip

--- init.lua
+++ init.lua
@@ -1,11 +1,8 @@
 require("core.mason-path")
-
 require("core.lsp")
-
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
 require("config.mason-verify")
 require("config.health-check")
-
 require("core.lazy")
