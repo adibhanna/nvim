025769e5588eb922094360bbe32c9844af9846e1
Adib Hanna <adibhanna@gmail.com>
2025-08-23 09:39:31 -0500

    wip

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
 require("core.mason-path")
 require("core.lsp")
 require("config.options")
-require("config.keymaps")
+require("config.keymaps") -- change 2
 require("config.autocmds")
 require("config.mason-verify")
-require("config.health-check")
+require("config.health-check") -- change 1
 require("core.lazy")
+
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -25,6 +25,7 @@ vim.opt.swapfile = false                                -- creates a swapfile
 vim.opt.termguicolors = true                            -- set term gui colors (most terminals support this)
 vim.opt.timeoutlen = 1000                               -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.undofile = true                                 -- enable persistent undo
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"     -- set an undo directory
 vim.opt.updatetime = 100                                -- faster completion (4000ms default)
 vim.opt.writebackup = false                             -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true                                -- convert tabs to spaces
