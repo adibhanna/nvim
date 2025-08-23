4fdfc925c052d3e76b0e551f9e71b8e4724f2685
Adib Hanna <adibhanna@gmail.com>
2025-08-23 09:39:46 -0500

    wip

--- init.lua
+++ init.lua
@@ -1,9 +1,8 @@
 require("core.mason-path")
 require("core.lsp")
 require("config.options")
-require("config.keymaps") -- change 2
+require("config.keymaps")
 require("config.autocmds")
 require("config.mason-verify")
-require("config.health-check") -- change 1
+require("config.health-check")
 require("core.lazy")
-
