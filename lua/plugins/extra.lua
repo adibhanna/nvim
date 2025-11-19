5fa58346574cae5700fb428cef1f85e6c17f68fe
Adib Hanna <adibhanna@gmail.com>
2025-11-18 22:31:26 -0600

    wip

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -84,38 +84,6 @@ return {
     },
   },
 
-  -- {
-  --   "utilyre/barbecue.nvim",
-  --   name = "barbecue",
-  --   version = "*",
-  --   dependencies = {
-  --     "SmiteshP/nvim-navic",
-  --     "nvim-tree/nvim-web-devicons", -- optional dependency
-  --   },
-  --   opts = {
-  --     -- configurations go here
-  --   },
-  --   config = function()
-  --     require("barbecue").setup({
-  --       create_autocmd = false, -- prevent barbecue from updating itself automatically
-  --     })
-
-  --     vim.api.nvim_create_autocmd({
-  --       "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
-  --       "BufWinEnter",
-  --       "CursorHold",
-  --       "InsertLeave",
-
-  --       -- include this if you have set `show_modified` to `true`
-  --       -- "BufModifiedSet",
-  --     }, {
-  --       group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-  --       callback = function()
-  --         require("barbecue.ui").update()
-  --       end,
-  --     })
-  --   end,
-  -- },
   -- persist sessions
   {
     "folke/persistence.nvim",
