ae0d736ac4169ebb6baeaae850abb61f35274321
Adib Hanna <adibhanna@gmail.com>
2025-11-21 11:42:48 -0600

    wip

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,5 +1,6 @@
 return {
   {
+    -- dir = "~/Developer/opensource/yukinord/neovim",
     "adibhanna/yukinord.nvim",
     config = function()
       vim.cmd("colorscheme yukinord")
@@ -541,7 +542,7 @@ return {
   {
     "projekt0n/github-nvim-theme",
     name = "github-theme",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Default options
@@ -550,14 +551,14 @@ return {
           -- Compiled file's destination location
           compile_path = vim.fn.stdpath("cache") .. "/github-theme",
           compile_file_suffix = "_compiled", -- Compiled file suffix
-          hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
-          hide_nc_statusline = true, -- Override the underline style for non-active statuslines
-          transparent = false, -- Disable setting bg (make neovim's background transparent)
-          terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-          dim_inactive = false, -- Non focused panes set to alternative background
-          module_default = true, -- Default enable value for modules
-          styles = { -- Style to be applied to different syntax groups
-            comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+          hide_end_of_buffer = true,         -- Hide the '~' character at the end of the buffer for a cleaner look
+          hide_nc_statusline = true,         -- Override the underline style for non-active statuslines
+          transparent = false,               -- Disable setting bg (make neovim's background transparent)
+          terminal_colors = true,            -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+          dim_inactive = false,              -- Non focused panes set to alternative background
+          module_default = true,             -- Default enable value for modules
+          styles = {                         -- Style to be applied to different syntax groups
+            comments = "NONE",               -- Value is any valid attr-list value `:help attr-list`
             functions = "NONE",
             keywords = "NONE",
             variables = "NONE",
